5afe0bf89b8a664968e7e768ad375cd08b2c79b9
George Wang <gwan6051@uni.sydney.edu.au>
2021-04-03 14:52:20 +1100

    LoginLogout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 import LogoutButton from './components/LogoutButton'
 import Home from './components/Home'
+import LoginButton from './components/LoginButton';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
-  return (
 
-    <div>
-      <Home />
-    </div>
-  );
+  const {isAuthenticated} = useAuth0();
+
+
+   if (isAuthenticated) {
+     return(
+      <div>
+        <LogoutButton />
+        <Home />
+      </div>
+     )
+   }
+   else{
+     return(
+     <div>
+      <LoginButton />
+     </div>
+     )
+   }
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,24 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import JSONPretty from 'react-json-pretty';
+//import JSONPretty from 'react-json-pretty';
 import Profile from './Profile'
 import styled from 'styled-components'
-import LoginButton from './LoginButton';
-import LogoutButton from './LogoutButton'
 
 const Home = () => {
     const { user, isAuthenticated } = useAuth0();
 
     return (
-        isAuthenticated && (
-            <div>
-                <Main>
-                    <Profile />  
-                    <Profile />  
-                </Main>
-                <img src={user.picture} alt={user.name} />
-                <h2>{user.name}</h2>
-                <p>{user.email}</p>
-                <JSONPretty data={user} />
-                {/* {JSON.stringify(user, null, 2)} */}
-            </div>
-        )
-        !=isAuthenticated && (
-            <div>
-                <LoginButton />
-                <LogoutButton />
-            </div>
-        )
+        <div>
+            <Main>
+                <Profile />  
+                <Profile />  
+            </Main>
+            <img src={user.picture} alt={user.name} />
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+            {/* <JSONPretty data={user} /> */}
+            {/* {JSON.stringify(user, null, 2)} */}
+        </div>
      )      
 }
 
