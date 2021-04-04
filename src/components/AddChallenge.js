3b258931f5907c62e6acd7e9f8c88f80d8ed0859
Dylan <dkro8507@uni.sydney.edu.au>
2021-04-05 05:20:36 +1000

    hi

--- src/components/AddChallenge.js
+++ src/components/AddChallenge.js
@@ -3,6 +3,12 @@ import './AddFriend1.css'
 import { EasybaseProvider, useEasybase } from 'easybase-react';
 import React, { useState, useEffect } from 'react';
 import './AddChallenge.css'
+import { IoIosWater } from "react-icons/io";
+import {IconContext} from "react-icons";
+import { BiWalk } from "react-icons/bi"
+import { GiGymBag } from "react-icons/gi"
+import {GiMeditation} from "react-icons/gi"
+
 
 const AddChallenge = ({ friendEmail }) => {
 
@@ -24,12 +30,14 @@ const AddChallenge = ({ friendEmail }) => {
     const sendData = () => {
 
         var button = document.querySelector("#Challenge_1");
-        
+        var button2 = document.querySelector("#Challenge_2");
+        var button3 = document.querySelector("#Challenge_3");
+        var button4 = document.querySelector("#Challenge_4");
+
         let challenge = button.value;
-        console.log(challenge);
 
         if (friendData.description != "") {
-            friendData.description = friendData.description + challenge + ",";
+            friendData.description = challenge + "," + friendData.description;
         } else {
             friendData.description = challenge + ",";
         }
@@ -37,15 +45,38 @@ const AddChallenge = ({ friendEmail }) => {
     }
     return (
             <Container>
-                <button class="butt" id={"Challenge_1"}onClick={sendData} value={"PushDown"}></button>
-                <button class="butt" onClick={sendData} value={"PushSide"}></button>
-                <button class="butt" onClick={sendData} value={"Push"}></button>
+                <Head>
+                    Send a wellness challenge to your friend. Costs 3 Bamboos.
+                </Head>
+                <B>
+            <IconContext.Provider value={{className: "icons"}}>
+
+                <button class="butt" id={"Challenge_1"}onClick={sendData} value={"CHALLENGE: Do 15 pushups"}><GiGymBag/></button>
+                <button class="butt" id={"Challenge_2"}onClick={sendData} value={"CHALLENGE: Drink 500mL of water"}><IoIosWater/></button>
+                <button class="butt" id={"Challenge_3"}onClick={sendData} value={"CHALLENGE: Talk a walk."}><BiWalk/></button>
+                <button class="butt" id={"Challenge_4"}onClick={sendData} value={"CHALLENGE: Meditate for 5 minutes"}><GiMeditation/></button>
+                </IconContext.Provider>
+                </B>
             </Container>
     )
 }
 
 export default AddChallenge
 
+const Head = styled.div`
+    font-weight: 600;
+    color: white;
+    margin-top:10px;
+    font-size: calc(0.9em + 10%);
+
+`
+
+const B = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    flex-direction: wrap;
+`
 
 const Container = styled.div`
     width: 45%;
@@ -62,6 +93,10 @@ const Container = styled.div`
     -moz-box-shadow: 0px 0px 27px -15px rgba(0,0,0,0.75);
     box-shadow: 0px 0px 27px -15px rgba(0,0,0,0.75);
     display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    flex-direction: wrap;
+    flex-direction: column;
 
     @media screen and (max-width: 1000px) {
      {
@@ -69,12 +104,4 @@ const Container = styled.div`
          left: 2.5%;
         right: 2.5%;
     }
-`
-
-const But1 = styled.div`
-    height: 100px;
-    width: 100px;
-    border-radius: 500px;
-    background-color: red;
-
 `
